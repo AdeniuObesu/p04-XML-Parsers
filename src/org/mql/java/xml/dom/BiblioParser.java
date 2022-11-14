405f64520f59a63ba3c1e323f08d126212b06418
MOUKHAFI Anass <moukhafi.anass@gmail.com>
2022-11-14 19:01:25 +0200

    Separated method main

--- src/org/mql/java/xml/dom/BiblioParser.java
+++ src/org/mql/java/xml/dom/BiblioParser.java
@@ -1,20 +1,28 @@
 package org.mql.java.xml.dom;
 
+import java.util.List;
+import java.util.Vector;
+
+import org.mql.java.xml.model.Document;
+
 public class BiblioParser {
+	List<Document> documents;
+	
 	public BiblioParser(String source) {
 		parse(source);
 	}
 	
 	void parse(String source) {
+		documents = new Vector<>();
 		XMLNode root = new XMLNode(source);
 		XMLNode children[] = root.children();
 		for(XMLNode child : children) {
 			int id = child.intAattribute("id");
-			System.out.println(id);
+			documents.add(new Document(id));
 		}
 	}
-	
-	public static void main(String[] args) {
-		new BiblioParser("resources/biblio.xml");
+
+	public List<Document> getDocuments() {
+		return documents;
 	}
 }
---
+++ src/org/mql/java/xml/dom/Main.java
@@ -0,0 +1,20 @@
+package org.mql.java.xml.dom;
+
+import java.util.List;
+
+import org.mql.java.xml.model.Document;
+
+public class Main {
+	public Main() {
+		exp01();
+	}
+	
+	void exp01() {
+		BiblioParser parser = new BiblioParser("resources/biblio.xml");
+		List<Document> docs = parser.getDocuments();
+		for(Document doc : docs) {
+			System.out.println(doc);
+		}
+		
+	}
+}
